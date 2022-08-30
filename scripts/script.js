1fb512c3348ab46d14df21fca6ce4af81ca42da3
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2022-08-29 23:44:31 -0300

    inciado a criação das media queries

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,19 @@
-var menu = document.getElementById('menu-mobile')
-
 function fechar() {
+    var menu = document.getElementById('menu-mobile')
+
     if (menu.checked == true) {
         menu.checked = false
+    }
+}
+
+function espaco() {
+    var logo = document.getElementById('logo')
+    var menu = document.getElementById('menu-mobile')
+    var largura = window.innerWidth
+
+    if (menu.checked == true && largura >= 992 ) {
+        logo.style.marginLeft = '5.9rem'
+    } else {
+        logo.style.marginLeft = '0'
     }
 }
